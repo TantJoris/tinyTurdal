a82cd8aa7198d8cdded6560d537a2c306cfaee82
Travis Joris <tjoris2018@bklawtech.com>
2017-02-16 20:50:09 +0000

    Tiny Turtle

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 TinyTurtle.apply(window); 
+
+
+/*global TinyTurtle*/
+
+right(50);
+forward(50);
+right(90);
+forward(50);
+right(130);
+forward(70);
+right(-90);
+forward(60);
+left(90);
+forward(70);
+left(90);
+forward(60);
+
+stamp();
